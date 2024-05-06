19d285d3ffc00616101c1db72f14683abd184de5
RamazanRahimli <162458672+RamazanRahimli@users.noreply.github.com>
2024-05-06 12:16:52 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import Router from "./Router/Router";
+import { MainContext } from "./Context/context";
+import axios from "axios";
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [student, setStudent] = useState("Haralisan  as");
+  const [color, setColor] = useState("red");
+
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products").then((res) => {
+      setProducts(res.data.products);
+    });
+  }, []);
+
+  const data = { products, setProducts, student, setStudent, setColor, color };
+
+  return (
+    <div>
+      <MainContext.Provider value={data}>
+        <Router />
+      </MainContext.Provider>
+    </div>
+  );
+};
+
+export default App;
